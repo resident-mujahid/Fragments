69dbf6286a49e0f3c9a92ce10f8057774f423236
Resident Mujahid <mujahidexpert@gmail.com>
2017-03-20 12:48:32 +0300

    final commit

---
+++ app/src/main/java/com/example/asus/fragments/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.asus.fragments;
+
+import android.app.FragmentManager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.view.Window;
+import android.widget.Button;
+
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.MoodButton)
+    Button moodButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        moodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+
+        });
+    }
+}
